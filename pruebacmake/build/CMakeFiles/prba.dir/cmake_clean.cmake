dfe548eb88e415b99bf38468903f09725febe0f8
Aaron <impre0hn@gmail.com>
2026-04-28 18:00:36 -0600

    a

---
+++ pruebacmake/build/CMakeFiles/prba.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prba.dir/link.d"
+  "CMakeFiles/prba.dir/main.cpp.o"
+  "CMakeFiles/prba.dir/main.cpp.o.d"
+  "prba"
+  "prba.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pruebacmake/main.cpp
@@ -0,0 +1,10 @@
+//cmake -B build -S . (CONFIGURE)
+//cmake --build build ()
+
+#include <iostream>
+
+int main(){
+
+    
+return 0;
+}
