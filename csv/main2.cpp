ce1ec11ce45f7deae298a8e34b7ff15debf5273d
Aaron <impre0hn@gmail.com>
2026-05-07 18:33:48 -0600

    7/06/2026

---
+++ csv/main2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <csv.h>
+#include <string>
+int main2(){
+    //Pone columnas
+    ios::CSVReader <3>in("students.csv")
+    in.read_header(io::ignore_etra_column,"nombre","edad","gpa");
+    std::string nombre;
+    int edad;
+    double gpa;
+
+    while (in.read_row(nombre,edad,gpa)){
+        std::cout << nombre << " " << gpa << "\n ";
+    }
+    return 0;
+}
