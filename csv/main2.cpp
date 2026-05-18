cca1c46ec9d15bca617b5dedbebb6245196d0e48
Aaron <impre0hn@gmail.com>
2026-05-17 22:52:45 -0600

    study = bad

--- csv/main2.cpp
+++ csv/main2.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <csv.h>
+#include "csv.h"
 #include <string>
-int main2(){
-    //Pone columnas
-    ios::CSVReader<3> in("students.csv")
-    in.read_header(io::ignore_extra_column,"nombre","edad","gpa");
-    std::string nombre;
-    int edad;
-    double gpa;
 
-    while (in.read_row(nombre,edad,gpa)){
-        std::cout << nombre << " " << gpa << "\n ";
+int main(){
+    //Pone columnas
+    io::CSVReader<3> in("students.csv");
+    in.read_header(io::ignore_extra_column, "Id", "Nombre", "Nota");
+
+    int id;
+    std::string nombre;
+    double nota;
+
+    while (in.read_row(id, nombre, nota)){
+        std::cout << "ID: " << id << " | Nombre: " << nombre << " | Nota: " << nota << "\n";
     }
+
     return 0;
 }
